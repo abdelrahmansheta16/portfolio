a0bb25c836b959abbbed241e891e1729e10d5368
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-09-03 10:14:07 +0300

    First Commit

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -56,7 +56,7 @@ const About = () => {
               </Heading>
             </div>
             <div className="about-text-desc">
-              <Text fontSize='3xl'>Experienced <span className="name">BLOCKCHAIN DEVELOPER</span> from Egypt, dedicated to the web3 industry. With 3+ years of expertise, I&apos;m passionate about innovation and continuous learning. Let&apos;s connect to drive blockchain solutions forward!</Text>
+              <Text fontSize='3xl'><span className="name">BLOCKCHAIN DEVELOPER</span> from Egypt, dedicated to the web3 industry. I&apos;m passionate about innovation and continuous learning. Let&apos;s connect to drive blockchain solutions forward!</Text>
             </div>
 
             <div className="icons">
